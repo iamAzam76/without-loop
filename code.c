751b5798d19463a5b85c19b040d4355dea1e183e
sharique015678 <73289867+sharique015678@users.noreply.github.com>
2020-10-22 17:27:44 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-int main() {
+void main() {
 int i=1;
 x:
 if(i<=10)
 {
-    printf("looping without any loop\n");
+    printf("looping without any loop :)\n");
     i++;
     goto x;
 }
-    return 0;
       getch();
 }
